8e1cef461221ebb59e272224b831c50f8a69b199
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-18 15:59:04 +0300

    Migrate readme yaml

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "bucket_domain_name" {
-  value = "${aws_s3_bucket.default.bucket_domain_name}"
+  value       = "${aws_s3_bucket.default.bucket_domain_name}"
+  description = "FQDN of bucket"
 }
 
 output "bucket_id" {
-  value = "${aws_s3_bucket.default.id}"
+  value       = "${aws_s3_bucket.default.id}"
+  description = "Bucket Name (aka ID)"
 }
 
 output "bucket_arn" {
-  value = "${aws_s3_bucket.default.arn}"
+  value       = "${aws_s3_bucket.default.arn}"
+  description = "Bucket ARN"
 }
 
 output "prefix" {
-  value = "${var.prefix}"
+  value       = "${var.prefix}"
+  description = "Prefix configured for lifecycle rules"
 }
