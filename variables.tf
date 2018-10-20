490d210006545fbb5faaadbbcdddb6a1b051b1e4
Lucas Pearson <lpearson@five-startech.com>
2018-10-20 16:53:03 -0400

    WIP

--- variables.tf
+++ variables.tf
@@ -100,3 +100,8 @@ variable "kms_master_key_id" {
   description = "The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms"
   default     = ""
 }
+
+variable "enabled" {
+  description = "Is module enabled"
+  default     = "true"
+}
