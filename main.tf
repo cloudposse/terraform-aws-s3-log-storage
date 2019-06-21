871cc9d79df370b1813b501b40b1b1e7a92eac39
rverma-nikiai <rohit.verma@niki.ai>
2019-06-21 19:31:31 +0530

    moving back pointer to cloudposse

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source     = "git::https://github.com/rverma-nikiai/terraform-null-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
   enabled    = var.enabled
   namespace  = var.namespace
   stage      = var.stage
