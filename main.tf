f0d3f6d7f88d76207e32535ab5b8ada777bd75b1
Igor Rodionov <goruha@gmail.com>
2019-08-13 15:42:31 +0600

    Get logging with s3 bucket

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_s3_bucket" "default" {
     }
   }
 
-  logging = "${null_resource.default.*.triggers}"
+  logging = "${compact(list(null_resource.default.*.triggers))}"
 
   tags = "${module.default_label.tags}"
 }
