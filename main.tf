2c2588c9e451b4f6dcf0fd103966c4fd54d48364
Adam Crews <adam.crews@gmail.com>
2019-12-18 13:11:24 -0800

    Update to latest label module to support the environment attribute (#22)
    * Update to latest label module to support the environment attribute
    
    * Refreshed README.md

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.1"
-  enabled    = var.enabled
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  enabled     = var.enabled
+  namespace   = var.namespace
+  environment = var.environment
+  stage       = var.stage
+  name        = var.name
+  delimiter   = var.delimiter
+  attributes  = var.attributes
+  tags        = var.tags
 }
 
 resource "aws_s3_bucket" "default" {
