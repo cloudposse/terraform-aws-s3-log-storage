fa9ed4a7da65bd087cb74b90d4bf0b5779f5aa60
Max Lobur <max_lobur@outlook.com>
2021-09-22 17:43:47 +0300

    set allow_ssl_requests_only to true by default

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -4,6 +4,6 @@ variable "region" {
 
 variable "allow_ssl_requests_only" {
   type        = bool
-  default     = false
+  default     = true
   description = "Set to `true` to require requests to use Secure Socket Layer (HTTPS/SSL). This will explicitly deny access to HTTP requests"
 }
--- variables.tf
+++ variables.tf
@@ -138,7 +138,7 @@ variable "allow_encrypted_uploads_only" {
 
 variable "allow_ssl_requests_only" {
   type        = bool
-  default     = false
+  default     = true
   description = "Set to `true` to require requests to use Secure Socket Layer (HTTPS/SSL). This will explicitly deny access to HTTP requests"
 }
 
