ebed5fd41dae0d8e4ed4b866dd6ccb3577c40b32
Max Lobur <max_lobur@outlook.com>
2021-09-22 16:57:21 +0300

    fix tests

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   type = string
 }
+
+variable "allow_ssl_requests_only" {
+  type        = bool
+  default     = false
+  description = "Set to `true` to require requests to use Secure Socket Layer (HTTPS/SSL). This will explicitly deny access to HTTP requests"
+}
