87bc24e7341dfe4049f4dc1cf7d2903c48e9ff2f
Igor Rodionov <goruha@gmail.com>
2025-04-02 21:48:33 +0300

    Update variables.tf

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,12 +3,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "allow_ssl_requests_only" {
-  type        = bool
-  default     = true
-  description = "Set to `true` to require requests to use Secure Socket Layer (HTTPS/SSL). This will explicitly deny access to HTTP requests"
-}
-
 variable "lifecycle_configuration_rules" {
   type = list(object({
     enabled = bool
